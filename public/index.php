6659b3f4505b85c96a7c27db028516f8cb308b78
Motaz <sassimotaze99@gmail.com>
2026-05-07 08:50:23 +0100

    deploy to railway

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use App\Kernel;
 
+// THE LAST VERSION OF THIS FILE CAN BE FOUND IN THE SYMFONY DEMO REPOSITORY:
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
