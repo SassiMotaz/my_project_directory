210db42e5863d5ac9d1d40663e596e1c83e9d874
Motaz <sassimotaze99@gmail.com>
2026-05-07 09:54:37 +0100

    fix railway composer install

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 // THE LAST VERSION OF THIS FILE CAN BE FOUND IN THE SYMFONY DEMO REPOSITORY:
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
